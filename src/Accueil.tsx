329ad5157d963fb726c0cf6c8384d0eb0b08e8c8
E204921Q <helori.belbeoch@etu.univ-nantes.fr>
2022-11-18 09:00:15 +0100

    improve

--- src/Accueil.tsx
+++ src/Accueil.tsx
@@ -8,7 +8,8 @@ const Accueil: React.FC = () => {
                 <h2>Page Accueil</h2>
                 <p> 
                     Bienvenu ! <br />
-                    Vous vous trouvez actuellement dans une page d'entrainement pour un projet fil-rouge. 
+                    Vous vous trouvez actuellement dans une page d'entrainement pour un projet fil-rouge. <br />
+                    Cette page contient un exemple d'utilisation de l'API : PokeAPI v2
                 </p>      
             </div>
         </div>
